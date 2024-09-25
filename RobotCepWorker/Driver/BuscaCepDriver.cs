33f4120508b7f094338634d30d2c2f2ea49cb7ca
Arthur Guelle <arthurguelle@gmail.com>
2024-09-24 23:06:41 -0300

    Incomclusivo

---
+++ RobotCepWorker/Driver/BuscaCepDriver.cs
@@ -0,0 +1,24 @@
+﻿using com.sun.security.ntlm;
+using EasyAutomationFramework;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotCepWorker.Driver
+{
+    private static readonly HttpClient endereco = new HttpClient();
+
+    public static async Task<EnderecoModel> BuscarEnderecoAsync(string cep)
+    {
+        string url = $"https://viacep.com.br/ws/{cep}/json/";
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        EnderecoModel endereco = JsonConvert.DeserializeObject<EnderecoModel>(responseBody);
+        return endereco;
+    }
+}
---
+++ RobotCepWorker/EnderecoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotCepWorker
+{
+    internal class EnderecoModel
+    {
+        public int Id { get; set; }
+        public string CEP { get; set; }
+        public string? Logadouro { get; set; }
+        public string? Bairro { get; set; }
+        public string? UF { get; set; }
+    }
+}
